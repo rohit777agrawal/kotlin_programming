1a226811b2e8b02cefa48ff480145608e5ba875a
Rohit Agrawal <rohit777agrawal@gmail.com>
2023-08-11 15:06:41 -0700

    Kotlin Learning!

---
+++ src/main/kotlin/loop_challenge.kt
@@ -0,0 +1,12 @@
+fun main(){
+    // print all the even numbers and keep count
+    var counter = 0
+    for(i in 1..20){
+        if((i%2) != 0){
+            continue
+        }
+        counter++
+        println(i)
+    }
+    println("The number of even numbers are: $counter")
+}
---
+++ src/main/kotlin/loops.kt
@@ -0,0 +1,28 @@
+fun main(){
+
+    //For Loops
+//    for(i in 1..10){   //to get the symbols, type 1..10
+//        println(i) //1-10
+//    }
+//
+//    for(i in 1 until 10){
+//        println(i)  //1-9
+//    }
+//
+//    for(i in 10 downTo 1){
+//        println(i)
+//    }
+//
+//    for(i in 1..10 step 2){
+//        println(i)
+//    }
+
+    //While loops
+    var number = 0
+//    while (number<10){
+//        number++
+//        println(number)
+//    }
+//    while (number<10) println(number++)  //Can be written in one sentences
+    while (number<10) println(++number)
+}
